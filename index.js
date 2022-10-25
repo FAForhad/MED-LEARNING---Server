95d5ce1a0a955a2e468c357a5cd4e4e51078dc19
FAForhad <faforhad450@gmail.com>
2022-10-25 11:21:16 +0600

    updated-json

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/instructors', (req, res) => {
     res.send(instructors)
 })
 
+app.get('/instructors/:id', (req, res) => {
+    const id = req.params.id
+    const singleInstructor = instructors.find(i => i.id === id)
+    res.send(singleInstructor)
+})
+
 app.listen(port, () => {
     console.log('app is running in port', port)
 })
